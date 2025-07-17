fcf03e07b17d05e4014f753443bbb1d01d653cde
sean sunly <156127901+seansunly@users.noreply.github.com>
2025-07-17 22:42:47 +0700

    sendmessage

--- vars/sendTelegramMessageV2.groovy
+++ vars/sendTelegramMessageV2.groovy
@@ -1,15 +1,18 @@
 def call(String message) {
-    withCredentials([usernamePassword(
-        credentialsId: 'TELEGRAM_BOT',
-        usernameVariable: 'CHAT_ID',
-        passwordVariable: 'TOKEN'
-    )]) {
-        def encodedMessage = URLEncoder.encode(message, "UTF-8")
-        sh """
-            curl -s -X POST "https://api.telegram.org/bot${TOKEN}/sendMessage" \\
-            -d chat_id=${CHAT_ID} \\
-            -d parse_mode=Markdown \\
-            -d text="${encodedMessage}" > /dev/null
-        """
-    }
+                
+    withCredentials([usernamePassword(credentialsId: 'TOKEN_TELEGRAM',
+    passwordVariable: 'TELEGRAM_TOKEN', usernameVariable: 'TELEGRAM_CHATID')]) {
+      def encodedMessage = URLEncoder.encode(message, "UTF-8")
+    sh """
+        curl -s -X POST https://api.telegram.org/bot${TELEGRAM_TOKEN}/sendMessage \\
+        -d chat_id=${TELEGRAM_CHATID} \\
+        -d text="${encodedMessage}" > /dev/null
+    """
+
+           
+        
+        
+    }     
+
+    
 }
