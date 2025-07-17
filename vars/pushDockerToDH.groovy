a77172c368e369c1216c50147e58b10940eb6c17
sean sunly <156127901+seansunly@users.noreply.github.com>
2025-07-17 23:09:22 +0700

    docker

--- vars/pushDockerToDH.groovy
+++ vars/pushDockerToDH.groovy
@@ -1,5 +1,5 @@
 def call(String imageName){
-    withCredentials([usernamePassword(credentialsId: 'DOCKERHUB', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {    
+    withCredentials([usernamePassword(credentialsId: 'DCOKERHUB', passwordVariable: 'PASSWORD', usernameVariable: 'USERNAME')]) {    
         sh """
             echo '$PASSWORD' |  docker login -u $USERNAME --password-stdin   
         docker push ${imageName}
